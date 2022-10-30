ed33aecf0e0f6e057c128bbc95d981ffbecbeb72
Kurayuri <Kurayuri@Kurayuri-LB04>
2022-10-30 13:58:44 +0800

    Add Aurora

---
+++ src/wrappers/aurora.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import os
+from os import path
+from subprocess import check_call
+
+import arg_parser
+import context
+
+
+def main():
+    args = arg_parser.receiver_first()
+
+    cc_repo = path.join(context.third_party_dir, 'aurora')
+    src_dir = path.join(cc_repo, 'src')
+    lib_dir = path.join(src_dir, 'core')
+    app_dir = path.join(src_dir, 'app')
+    send_src = path.join(app_dir, 'pccclient')
+    recv_src = path.join(app_dir, 'pccserver')
+
+    if args.option == 'setup':
+        check_call(['make'], cwd=src_dir)
+        return
+
+    if args.option == 'receiver':
+        os.environ['LD_LIBRARY_PATH'] = path.join(lib_dir)
+        cmd = [recv_src, 'recv', args.port]
+        check_call(cmd)
+        return
+
+    if args.option == 'sender':
+        os.environ['LD_LIBRARY_PATH'] = path.join(lib_dir)
+        # cmd = [send_src, 'send', args.ip, args.port]
+        
+        # aurora
+        cmd = [send_src, 'send', args.ip, args.port, 
+            "--pcc-rate-control=python",
+            "-pyhelper=loaded_client",
+            "-pypath=/home/kurayuri/pantheon/third_party/aurora/PCC-RL/src/udt-plugins/testing/",
+            "--history-len=10",
+            "--pcc-utility-calc=linear",
+            "--model-path=/home/kurayuri/pantheon/third_party/aurora/model/origin"
+            # "--model-path=/home/kurayuri/pantheon/third_party/aurora/model/aurora_withp3"
+            # "--model-path=/home/kurayuri/pantheon/third_party/aurora/model/aurora_trained"
+        ]
+        print(" ".join(cmd))
+        check_call(cmd)
+        return
+
+
+if __name__ == '__main__':
+    main()
